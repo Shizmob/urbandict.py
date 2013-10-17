72c92d124020b7dac73423c17d512da41e4424c0
Shiz <mark@xn--hwg34fba.ws>
2013-10-17 20:16:59 +0200

    Accidently committed outdated file.

--- urbandict.py
+++ urbandict.py
@@ -5,25 +5,40 @@ import requests
 API_BASE = 'http://api.urbandictionary.com/v0/'
 
 def define(term):
+    """ Get definitions for `term`. """
     try:
         res = requests.get(API_BASE + 'define', params={'term': term}).json()
     except:
         return None
     
-    if res['results_type'] == 'no_results':
+    if res['result_type'] == 'no_results':
         return None
 
     return {
         'tags': res['tags'],
-        'definitions': [{
-            'id': item['defid'],
-            'term': item['word']
-            'definition': item['definition'],
-            'author': item['author'],
-            'up': item['thumbs_up'],
-            'down': item['thumbs_down'],
-            'score': item['thumbs_up'] - item['thumbs_down'],
-            'link': item['permalink']
-        } for item in res['list']]
+        'definitions': [ parse_definition(item) for item in res['list'] ]
     }
 
+def random():
+    """ Get a random definition for a random term. """
+    import random
+
+    try:
+        res = requests.get(API_BASE + 'random').json()
+    except:
+        return None
+
+    return parse_definition(random.choice(res['list']))
+
+
+def parse_definition(item):
+    return {
+        'id': item['defid'],
+        'term': item['word'],
+        'definition': item['definition'],
+        'author': item['author'],
+        'up': item['thumbs_up'],
+        'down': item['thumbs_down'],
+        'score': item['thumbs_up'] - item['thumbs_down'],
+        'link': item['permalink']
+    } 
